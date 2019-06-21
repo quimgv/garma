cd75bdef364bf353de9a9000cfe38f5a4e17e413
Quim Granados <quimgranados@gmail.com>
2019-06-21 16:01:01 +1000

    add helper functions with handleServerErrors

---
+++ client/src/utils/helperFunctions.js
@@ -0,0 +1,17 @@
+export const handleServerErrors = (err, dispatch, setAlert) => {
+    if (err.response.data.showErr) {
+      let errors = err.response.data.showErr;
+  
+      const errorsArr = Object.keys(errors);
+      errorsArr.forEach(errorKey => {
+        dispatch(setAlert(errors[errorKey], "danger"));
+      });
+    } else {
+      dispatch(
+        setAlert(
+          "Upss, something wrong has happened. Please contact us",
+          "danger"
+        )
+      );
+    }
+  }

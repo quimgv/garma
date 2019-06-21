acb6c704447afcd85126c48e483e950700f4ab9b
Quim Granados <quimgranados@gmail.com>
2019-06-21 15:53:40 +1000

    favoursListItem component

---
+++ client/src/components/FavoursListItem.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Card, Badge } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const FavoursListItem = ({
+  _id,
+  title,
+  description,
+  value,
+  categories,
+  status
+}) => {
+  let statusVariant;
+
+  switch (status) {
+    case "Open":
+      statusVariant = "success";
+      break;
+    case "In progress":
+      statusVariant = "warning";
+      break;
+    case "Completed":
+      statusVariant = "primary";
+      break;
+    default:
+      statusVariant = "primary";
+  }
+  return (
+    <Card className="mb-4 text-center">
+      <Card.Header className="header">{title}</Card.Header>
+      <Card.Body>
+        <Card.Text>{description}</Card.Text>
+
+        <div className="d-flex justify-content-between align-items-center">
+          <Badge pill variant={statusVariant} className="mr-2">
+            {status}
+          </Badge>
+          <div className="score">{value}</div>
+          <div>
+            <Link to={`/favour/${_id}`}>See Favour -></Link>
+          </div>
+        </div>
+      </Card.Body>
+      <Card.Footer className="text-muted">
+        {categories.map((category, idx) => (
+          <span key={category}>{"#" + category}</span>
+        ))}
+      </Card.Footer>
+    </Card>
+  );
+};
+
+export default FavoursListItem;

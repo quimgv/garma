6949e2a477eec8d1b2dcdfbb70fcd60673f69ab5
Quim Granados <quimgranados@gmail.com>
2019-06-21 16:01:55 +1000

    createFavourForm component

--- client/src/components/CreateFavourForm.js
+++ client/src/components/CreateFavourForm.js
@@ -1,101 +1,259 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col, Button, Form, FormControl } from "react-bootstrap";
-import { Formik } from "formik";
-import * as Yup from "yup";
 
-const CreateFavourSchema = Yup.object().shape({
-  title: Yup.string()
-    .max(100, "Too Long!")
-    .required("Required"),
-  description: Yup.string()
-    .max(500, "Too Long!")
-    .required("Required"),
-  value: Yup.number()
-    .typeError("This field must be a number")
-    .positive("Number must be positive")
-    .required("Required")
-});
+// Redux
+import { connect } from "react-redux";
+import { createFavour } from "../redux/actions/favour";
 
-const CreateFavourForm = ({ handleModal, ...props }) => {
+// Validation
+import { createFavourValidation } from "../validation/formsValidation";
 
-  const handleCreateFavour = values => {
-    console.log(values);
+const CreateFavourForm = ({ handleModal, createFavour }) => {
+  const initialState = {
+    title: "",
+    description: "",
+    value: "",
+    urgency: "",
+    difficulty: "",
+    categories: [],
+    titleError: "",
+    descriptionError: "",
+    valueError: "",
+    urgencyError: "",
+    difficultyError: "",
+    categoriesError: "",
+    categoriesControl: [
+      {
+        name: "Physical",
+        isChecked: false
+      },
+      {
+        name: "Financial",
+        isChecked: false
+      },
+      {
+        name: "Advice",
+        isChecked: false
+      },
+      {
+        name: "Transport",
+        isChecked: false
+      },
+      {
+        name: "Small favour",
+        isChecked: false
+      },
+      {
+        name: "Social media",
+        isChecked: false
+      },
+      {
+        name: "Other",
+        isChecked: false
+      }
+    ]
+  };
+
+  const [formData, setFormData] = useState(initialState);
+
+  const {
+    title,
+    description,
+    value,
+    urgency,
+    difficulty,
+    categories,
+    titleError,
+    descriptionError,
+    valueError,
+    urgencyError,
+    difficultyError,
+    categoriesControl
+  } = formData;
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // Form Validation
+    const { errors, isValid } = createFavourValidation(
+      title,
+      description,
+      value,
+      urgency,
+      difficulty
+    );
+
+    const resetErrors = {
+      titleError: "",
+      descriptionError: "",
+      valueError: "",
+      urgencyError: "",
+      difficultyError: ""
+    };
+
+    if (!isValid) {
+      return setFormData({ ...formData, ...resetErrors, ...errors });
+    }
+
+    createFavour({
+      title,
+      description,
+      value,
+      urgency,
+      difficulty,
+      categories
+    });
+    setFormData(initialState);
     handleModal();
+  };
+
+  const handleOnChange = e => {
+    if (e.target.type === "checkbox") {
+      // Check control categories
+      categoriesControl.forEach((category, idx) => {
+        if (e.target.name === category.name) {
+          categoriesControl[idx].isChecked = !category.isChecked;
+        }
+      });
+
+      // Edit categories field
+      if (categories.includes(e.target.name)) {
+        const indexCat = categories.indexOf(e.target.name);
+        categories.splice(indexCat, 1);
+      } else {
+        categories.push(e.target.name);
+      }
+      setFormData({ ...formData, categories, categoriesControl });
+    } else {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    }
   };
 
   return (
     <div>
-      <Formik
-        initialValues={{ title: "", description: "", value: "" }}
-        validationSchema={CreateFavourSchema}
-        onSubmit={handleCreateFavour}
-        render={props => (
-          <Form onSubmit={props.handleSubmit}>
-            <Form.Group>
-              <Form.Label>Favour Title</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Add title"
-                onChange={props.handleChange}
-                onBlur={props.handleBlur}
-                value={props.values.title}
-                name="title"
-                isValid={!!props.values.title && !props.errors.title}
-                isInvalid={!!props.errors.title}
-              />
-              <FormControl.Feedback type="invalid">
-                <div>{props.errors.title}</div>
-              </FormControl.Feedback>
-            </Form.Group>
+      <Form onSubmit={(e, values) => handleSubmit(e, values)}>
+        <Form.Group>
+          <Form.Label>Favour Title</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Add title"
+            onChange={handleOnChange}
+            value={title}
+            name="title"
+            isValid={!!titleError}
+            isInvalid={titleError}
+          />
+          <FormControl.Feedback type="invalid">
+            <div>{titleError}</div>
+          </FormControl.Feedback>
+        </Form.Group>
 
-            <Form.Group>
-              <Form.Label>Favour Description</Form.Label>
-              <Form.Control
-                as="textarea"
-                rows="3"
-                placeholder="Add description"
-                onChange={props.handleChange}
-                onBlur={props.handleBlur}
-                value={props.values.description}
-                name="description"
-                isValid={
-                  !!props.values.description && !props.errors.description
-                }
-                isInvalid={!!props.errors.description}
-              />
-              <FormControl.Feedback type="invalid">
-                <div>{props.errors.description}</div>
-              </FormControl.Feedback>
-            </Form.Group>
+        <Form.Group>
+          <Form.Label>Favour Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows="3"
+            placeholder="Add description"
+            onChange={handleOnChange}
+            value={description}
+            name="description"
+            isValid={!!descriptionError}
+            isInvalid={descriptionError}
+          />
+          <FormControl.Feedback type="invalid">
+            <div>{descriptionError}</div>
+          </FormControl.Feedback>
+        </Form.Group>
 
-            <Form.Group>
-              <Form.Label>Value</Form.Label>
-              {console.log(props)}
-              <Form.Control
-                type="text"
-                placeholder="10"
-                onChange={props.handleChange}
-                onBlur={props.handleBlur}
-                value={props.values.value}
-                name="value"
-                isValid={!!props.values.value && !props.errors.value}
-                isInvalid={!!props.errors.value}
-              />
-              <FormControl.Feedback type="invalid">
-                <div>{props.errors.value}</div>
-              </FormControl.Feedback>
-            </Form.Group>
+        <Form.Group>
+          <Form.Label>Value</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="10"
+            onChange={handleOnChange}
+            value={value}
+            name="value"
+            isValid={!!valueError}
+            isInvalid={valueError}
+          />
+          <FormControl.Feedback type="invalid">
+            <div>{valueError}</div>
+          </FormControl.Feedback>
+        </Form.Group>
 
-            <div className="text-center">
-              <Button variant="primary" type="submit">
-                Create favour
-              </Button>
-            </div>
-          </Form>
-        )}
-      />
+        <Form.Group>
+          <Form.Label>Urgency</Form.Label>
+          <Form.Control
+            as="select"
+            name="urgency"
+            onChange={handleOnChange}
+            isValid={!!urgencyError}
+            isInvalid={urgencyError}
+          >
+            <option>Choose...</option>
+            <option>Urgent</option>
+            <option>Within week</option>
+            <option>Within month</option>
+            <option>Anytime</option>
+          </Form.Control>
+          <FormControl.Feedback type="invalid">
+            <div>{urgencyError}</div>
+          </FormControl.Feedback>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Difficulty</Form.Label>
+          <Form.Control
+            as="select"
+            name="difficulty"
+            onChange={handleOnChange}
+            isValid={!!difficultyError}
+            isInvalid={difficultyError}
+          >
+            <option>Choose...</option>
+            <option>Easy</option>
+            <option>Medium</option>
+            <option>Anytime</option>
+          </Form.Control>
+          <FormControl.Feedback type="invalid">
+            <div>{difficultyError}</div>
+          </FormControl.Feedback>
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Categories</Form.Label>
+          <Row>
+            <Col>
+              {categoriesControl.map((category, idx) => (
+                <Form.Check
+                  key={idx}
+                  inline
+                  type="checkbox"
+                  name={category.name}
+                  label={category.name}
+                  checked={category.isChecked}
+                  onChange={handleOnChange}
+                />
+              ))}
+            </Col>
+          </Row>
+
+          <FormControl.Feedback type="invalid">
+            <div>{valueError}</div>
+          </FormControl.Feedback>
+        </Form.Group>
+
+        <div className="text-center">
+          <Button variant="primary" type="submit">
+            Create favour
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 };
 
-export default CreateFavourForm;
+export default connect(
+  null,
+  { createFavour }
+)(CreateFavourForm);

d3007e11e33c779459f9373aa9d5fe972605465c
Quim Granados <quimgranados@gmail.com>
2019-06-22 13:08:14 +1000

    added isOwner isHelper and favourStatusVariant

--- client/src/utils/helperFunctions.js
+++ client/src/utils/helperFunctions.js
@@ -1,17 +1,42 @@
 export const handleServerErrors = (err, dispatch, setAlert) => {
-    if (err.response.data.showErr) {
-      let errors = err.response.data.showErr;
-  
-      const errorsArr = Object.keys(errors);
-      errorsArr.forEach(errorKey => {
-        dispatch(setAlert(errors[errorKey], "danger"));
-      });
-    } else {
-      dispatch(
-        setAlert(
-          "Upss, something wrong has happened. Please contact us",
-          "danger"
-        )
-      );
-    }
+  if (err.response.data.showErr) {
+    let errors = err.response.data.showErr;
+
+    const errorsArr = Object.keys(errors);
+    errorsArr.forEach(errorKey => {
+      dispatch(setAlert(errors[errorKey], "danger"));
+    });
+  } else {
+    dispatch(
+      setAlert(
+        "Upss, something wrong has happened. Please contact us",
+        "danger"
+      )
+    );
   }
+};
+
+export const favourStatusVariant = status => {
+  switch (status) {
+    case "Open":
+      return "success";
+    case "In progress":
+      return "warning";
+    default:
+      return "primary";
+  }
+};
+
+export const isOwner = (favourOwnerId, currentUserId) => {
+  if (favourOwnerId === currentUserId) {
+    return true;
+  }
+  return false;
+};
+
+export const isHelper = (favourHelperId, currentUserId) => {
+  if (favourHelperId === currentUserId) {
+    return true;
+  }
+  return false;
+};

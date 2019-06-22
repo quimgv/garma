9458b109f4ffeb44a0231a0346634ef230d5d5a1
Quim Granados <quimgranados@gmail.com>
2019-06-22 14:14:23 +1000

    update with favourStatus and favourItemActions

--- client/src/components/FavourItem/FavourItem.js
+++ client/src/components/FavourItem/FavourItem.js
@@ -1,28 +1,31 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment } from "react";
 
 // Redux
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 // Components
-import Loader from '../Common/Loader';
-import OwnAssigned from '../FavourItem/OwnAssignedBadge';
+import Loader from "../Common/Loader";
+import OwnAssigned from "../FavourItem/OwnAssignedBadge";
+import FavourStatus from "../FavourItem/FavourStatus";
+import FavourItemActions from '../FavourItem/FavourItemActions';
 
 const FavourItem = ({ favour, favourLoading }) => {
-
   if (favourLoading) {
     return <Loader />;
-  } else if(favour) {
+  } else if (favour) {
+    const { title, description, value } = favour;
     return (
       <Fragment>
         <div className="card mb-4">
           <div className="card-body">
             <div className="card-header d-flex justify-content-between">
-              <h5 className="card-title">{favour.title}</h5>
+              <h5 className="card-title">{title}</h5>
               <OwnAssigned />
             </div>
-            <p>{favour.description}</p>
-            <p>Value: 10</p>
-            <p>Status: Completed</p>
+            <p>{description}</p>
+            <p>Value: {value}</p>
+            <FavourStatus />
+            <FavourItemActions />
           </div>
         </div>
       </Fragment>
@@ -30,13 +33,11 @@ const FavourItem = ({ favour, favourLoading }) => {
   } else {
     return <div />;
   }
-
-  
 };
 
 const mapStateToProps = state => ({
   favour: state.favour.favours[0],
   favourLoading: state.favour.loading
-})
+});
 
 export default connect(mapStateToProps)(FavourItem);

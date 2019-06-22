cf730d608374430b081baa8497a39d4b45822e66
Quim Granados <quimgranados@gmail.com>
2019-06-22 13:09:32 +1000

    updated with isOnwer isHelper helper functions

--- client/src/components/FavourItem/OwnAssignedBadge.js
+++ client/src/components/FavourItem/OwnAssignedBadge.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Badge } from "react-bootstrap";
-import './OwnAssignedBadge.css';
+import "./OwnAssignedBadge.css";
+import { isOwner, isHelper } from "../../utils/helperFunctions";
 
 // Redux
 import { connect } from "react-redux";
 
 const OwnAssigned = ({ favour, user }) => {
-    // console.log(favour.owner.user._id, user._id)
+  // console.log(favour.owner.user._id, user._id)
   if (user && favour) {
-    if (favour.owner.user._id === user._id) {
+    if (isOwner(favour.owner.user._id, user._id)) {
       return (
         <Badge pill variant="primary" className="mt-2 mr-2 ownBadge">
           Own
         </Badge>
       );
-    } else if (favour.helper.user && favour.helper.user._id === user._id) {
+    } else if (favour.helper.user && isHelper(favour.helper.user, user._id)) {
       return (
         <Badge pill variant="primary" className="mt-2 mr-2 ownBadge">
           Assigned
